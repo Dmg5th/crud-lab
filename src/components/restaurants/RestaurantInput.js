604a2dc44f6aac670ea61700c876ddb69154a628
Dmg5th <dmgould310@gmail.com>
2021-02-19 15:07:02 -0500

    commit tree fb716df78dce25d91ef0d19970502019baba8cb8

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value, 
+    }); 
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addRestaurant(this.state.text); 
+    this.setState({
+      text: ""
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+            <input onChange={this.handleChange} type="text" value={this.state.text}></input>
+            <input type="submit"></input>
+        </form>
       </div>
     );
   }
